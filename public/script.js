60bd3620e6b3d86dd652a2ba7e9f356c789e9ad2
Glitch (melodious-geode-snapdragon) <none>
2021-01-17 00:43:43 +0000

    👻🚊 Updated with Glitch

---
+++ public/script.js
@@ -0,0 +1,38 @@
+function counter() {
+  let end = new Date("May 10, 2020 17:00:00").getTime();
+  let now = new Date().getTime();
+  let time = Math.floor((end - now) / 1000);
+  document.getElementById("numbers").textContent = time.toLocaleString();
+}
+counter();
+setInterval(counter, 1000);
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
